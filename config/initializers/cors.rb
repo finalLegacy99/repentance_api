53c6a68a638113f331f3be39f622ecdebd71cb05
finalLegacy <ffeightbest@gmail.com>
2022-04-19 20:08:11 -0500

    secure site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://repentance.netlify.app/', 'https://repentance-editor.netlify.app/'
 
     resource '*',
       headers: :any,
